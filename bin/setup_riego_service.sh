acd7d7673186d0881f3e8b58f405a899c578505d
py-steph <github.com@universalmail.de>
2021-02-05 19:46:41 +0000

    lost bin directory

---
+++ bin/setup_riego_service.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+
+sudo bash -c "cat > /lib/systemd/system/riego.service" <<'EOT'
+[Unit]
+Description=Riego Rain-System
+After=mnt-usb1.mount mosquitto.service
+StartLimitIntervalSec=500
+StartLimitBurst=5
+
+[Service]
+Environment="PYTHONUNBUFFERED=1"
+Type=simple
+User=riego
+WorkingDirectory=/mnt/usb1/riego
+ExecStart=/mnt/usb1/riego/.venv/bin/riego
+Restart=always
+RestartSec=3s
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
+systemctl daemon-reload
+systemctl enable riego
+systemctl restart riego
