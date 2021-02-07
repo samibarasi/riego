4d88f3195641f60ef4721c07e091e4cdad38cc4e
py-steph <github.com@universalmail.de>
2021-02-07 15:16:02 +0000

    Reverse sorting

--- riego/web/views/events.py
+++ riego/web/views/events.py
@@ -17,7 +17,8 @@ async def system_event_log(request):
     c = get_db().conn.cursor()
     c.execute('''SELECT events.*, valves.name
                 FROM events, valves
-                WHERE events.valve_id = valves.id''')
+                WHERE events.valve_id = valves.id
+                ORDER BY events.created_at DESC''')
     items = c.fetchall()
     get_db().conn.commit()
     return {'items': items}
